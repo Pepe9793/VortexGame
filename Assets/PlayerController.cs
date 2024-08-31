2599808c6cece63d5de5de1a0d0f24605a6a84b9
ROHAN KUSHWAHA <kushwaharohan2003@gmail.com>
2024-08-31 16:26:50 +0530

    Adding Input and Camera Follow

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 2.5f;
+    public float rotationSpeed = 1.0f;
     
     void Start()
     {
@@ -15,5 +17,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         transform.Translate(0, 0, speed * Time.deltaTime);
+        if (Touchscreen.current != null)
+        {
+            Vector2 delta = Touchscreen.current.primaryTouch.delta.ReadValue();
+            transform.Rotate(0, 0, delta.x * rotationSpeed);
+        }
     }
 }
